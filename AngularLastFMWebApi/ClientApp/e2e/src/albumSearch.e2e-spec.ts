be9b336e7749116a5fcffcad51e3c39c3a329506
Katsiaryna_Auchynnikava <Katsiaryna_Auchynnikava@epam.com>
2018-08-10 17:42:32 +0300

    Add FE e2e tests.

---
+++ AngularLastFMWebApi/ClientApp/e2e/src/albumSearch.e2e-spec.ts
@@ -0,0 +1,25 @@
+﻿import { browser, by, element } from 'protractor';
+
+describe('album search', () => {
+
+  function navigateTo() {
+    browser.waitForAngularEnabled(false);
+    return browser.get('/albums');
+  }
+
+  it('should display album name', () => {
+    navigateTo();
+    var artistNameSearchInput = element(by.id("lblAlbumNameSearch"));
+    artistNameSearchInput.clear().then(function () {
+      artistNameSearchInput.sendKeys('love');
+    });
+
+    var artistNameSearchButton = element(by.id("btnAlbumNameSearch"));
+    artistNameSearchButton.click();
+    browser.sleep(8000);
+    var cardItemName = element(by.className('card__itemname'));
+    cardItemName.getText().then(function (text) {
+      expect(text.toUpperCase()).toContain('LOVE');
+    });
+  });
+});
---
+++ AngularLastFMWebApi/ClientApp/e2e/src/artistSearch.e2e-spec.ts
@@ -0,0 +1,22 @@
+﻿import { browser, by, element } from 'protractor';
+
+describe('artist search', () => {
+
+  function navigateTo() {
+    browser.waitForAngularEnabled(false);
+    return browser.get('/artists');
+  }
+
+  it('should display artist name', () => {
+    navigateTo();
+    var artistNameSearchInput = element(by.id("lblArtistNameSearch"));
+    artistNameSearchInput.clear().then(function () {
+      artistNameSearchInput.sendKeys('Cher');
+    });
+
+    var artistNameSearchButton = element(by.id("btnArtistNameSearch"));
+    artistNameSearchButton.click();
+    browser.sleep(8000); 
+    expect(element(by.className('card__itemname')).getText()).toEqual('CHER');
+  });
+});
